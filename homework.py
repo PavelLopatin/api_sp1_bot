db964f79cf93ebbf1d8f9350822ebaf9585c90af
Павел <lopatyn1244@gmail.com>
2021-05-12 01:53:57 +0500

    исправления

--- homework.py
+++ homework.py
@@ -8,7 +8,8 @@ import logging
 
 load_dotenv()
 
-
+TIME = time.sleep(300)
+TIME_ER = time.sleep(15)
 PRAKTIKUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
@@ -16,14 +17,16 @@ PRAKTIKUM_URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
 
 
 def parse_homework_status(homework):
+    status = {'reviewing': 'Работа взята в ревью',
+              'approved': 'Ревьюеру всё понравилось,'
+                          ' можно приступать к следующему уроку',
+              'rejected': 'К сожалению в работе нашлись ошибки'}
     homework_name = homework['homework_name']
-    status = homework['status']
-    if status == 'rejected':
-        verdict = 'К сожалению в работе нашлись ошибки.'
-    else:
-        verdict = 'Ревьюеру всё понравилось,' \
-                  ' можно приступать к следующему уроку.'
-    return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
+    verdict = status[homework['status']]
+    if homework_name is None or status is None:
+        logging.error('Неверный статус')
+        return 'Неверный статус'
+    return f'У вас проверили работу "{homework_name}"!\n{verdict}.'
 
 
 def get_homework_statuses(current_timestamp):
@@ -34,7 +37,7 @@ def get_homework_statuses(current_timestamp):
             headers={'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'})
         logging.info('Запрос удачный')
         return homework_statuses.json()
-    except Exception as error:
+    except requests.RequestException as error:
         logging.exception(error)
 
 
@@ -46,7 +49,6 @@ def send_message(message, bot_client):
 def main():
     bot_client = telegram.Bot(token=TELEGRAM_TOKEN)
     current_timestamp = int(time.time())
-    logging.debug('Бот инициализизован.')
     while True:
         try:
             new_homework = get_homework_statuses(current_timestamp)
@@ -58,12 +60,12 @@ def main():
                 'current_date',
                 current_timestamp)
             logging.info('Дату поменяли.')
-            time.sleep(300)
+            TIME
         except Exception as e:
             logging.error(e, exc_info=True)
             text = f'Бот столкнулся с ошибкой: {e}'
             bot_client.send_message(chat_id=CHAT_ID, text=text)
-            time.sleep(5)
+            TIME_ER
 
 
 if __name__ == '__main__':
